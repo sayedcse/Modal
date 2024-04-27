3d5c4a47d2cdbf8f718bfb5788cfb7aae31c0c60
sayedcse <saidursayed1@gmail.com>
2024-04-27 20:05:48 +0600

    Key event added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 'use strict';
+//Variables for class selector
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
+//Functions for open & close modal
 const showModal = () => {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
@@ -16,3 +18,9 @@ for (let i = 0; i < btnShowModal.length; i++) {
 }
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+//Key press function
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
